34cd3b67ff9829d4412c56061eb2fa1851d26ee5
S-yshzk <chsu21046@g.nihon-u.ac.jp>
2023-12-17 23:45:53 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import '../style.css'
+function App() {
+    const japan = [35.6895, 139.6917];
+    return (
+        <MapContainer center={japan} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={japan}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    );
+  }
+  
+  export default App;
