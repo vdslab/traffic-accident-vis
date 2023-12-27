21779566ec9d10945f96ab7fc687b9b366abb1fa
Ishi <syi378039@gmail.com>
2023-12-27 23:49:02 +0900

    ヘッダー作った。

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header'
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <>
-      <p>Hello</p>
+      <Header />
+      <p>Hello world</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <p>Count: {count}</p>
     </>
   )
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+  <>
+    <header className="header">
+      <h2>Traffic Accident Vis</h2>
+    </header>
+  </>
+  );
+}
+
+export default Header;
