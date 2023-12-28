539bd08b1baca3ee0d128b1fcd358720edcad1ca
Ishi <syi378039@gmail.com>
2023-12-28 14:51:29 +0900

    ボタンの修正

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 
 const Button = (props) => {
-	const [color, setColor] = useState('blue');
+  const defaultColor = ['rgb(25, 118, 210)', 'rgb(149, 150, 151)'];
+  const [color, setColor] = useState(defaultColor[0]);
 
-	const handleClick = () => color === 'red' ? setColor('blue') : setColor('red');
+  const handleClick = () => {
+    const newColor = color === defaultColor[1] ? defaultColor[0] : defaultColor[1];
+    setColor(newColor);
+  };
 
-	return (
-		<button style={{ backgroundColor: color }} onClick={handleClick}>
-			{props.text}
-		</button>
-	);
+  return (
+    <button className="selectBtn" style={{ backgroundColor: color }} onClick={handleClick}>
+	    {props.text}
+    </button>
+  );
 };
 
 export default Button;
