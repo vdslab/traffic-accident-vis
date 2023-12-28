546607e0c15e84ca1715a41b59a8cb12092d60b7
Ishi <syi378039@gmail.com>
2023-12-28 14:53:17 +0900

    PATHの修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/header/header'
+import Header from './components/header'
 import Button from './components/button'
 
 function App() {
