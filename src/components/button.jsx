e264c49abb14f3ae98a4e78882132fe88b9ed41f
Ishi <syi378039@gmail.com>
2024-01-11 14:46:27 +0900

    チェックボックスにcss当てました。

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,19 +1,44 @@
 import React, { useState, useContext } from 'react';
 import { AccidentTypeContext } from '../App.jsx';
 
+// const Button = ({ data, i, j }) => {
+//   const [accidentInfo, setAccidentInfo] = useContext(AccidentTypeContext);
+//   return (
+//     <div className='selectBtn'>
+//       <label>
+//         <input type="checkbox" checked={data.checked} onChange={() => {
+//           const newChecked = !data.checked;
+//           const newAccidentInfo = [...accidentInfo];
+//           newAccidentInfo[i].types[j].checked = newChecked;
+//           setAccidentInfo(newAccidentInfo);
+//         }} />
+//         {data.type}
+//       </label>
+//     </div>
+//   );
+// }
+
 const Button = ({ data, i, j }) => {
   const [accidentInfo, setAccidentInfo] = useContext(AccidentTypeContext);
+
   return (
-    <label>
-      <input type="checkbox" className="selectBtn" checked={data.checked} onChange={() => {
-        const newChecked = !data.checked;
-        const newAccidentInfo = [...accidentInfo];
-        newAccidentInfo[i].types[j].checked = newChecked;
-        setAccidentInfo(newAccidentInfo);
-      }} />
-      {data.type}
-    </label>
+    <div className={`selectBtn ${data.checked ? 'checked' : ''}`}>
+      <label>
+        <input
+          type="checkbox"
+          checked={data.checked}
+          onChange={() => {
+            const newChecked = !data.checked;
+            const newAccidentInfo = [...accidentInfo];
+            newAccidentInfo[i].types[j].checked = newChecked;
+            setAccidentInfo(newAccidentInfo);
+          }}
+        />
+        {data.type}
+      </label>
+    </div>
   );
-}
+};
+
 
 export default Button;
