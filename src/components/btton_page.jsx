19cce0c780d504942b7706d299c637d0bbd9a40d
Ishi <syi378039@gmail.com>
2024-01-11 12:32:24 +0900

    console.logの削除

--- src/components/btton_page.jsx
+++ src/components/btton_page.jsx
@@ -18,7 +18,6 @@ const AccidentTypeSelector = ({ title, typeArray, i }) => (
 
 const ButtonPage = () => {
   const [accidentInfo] = useContext(AccidentTypeContext);
-  // console.log(accidentInfoState);
   return (
     <>
       <div className="BtnPage">
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,16 +2,13 @@ import React, { useState, useContext } from 'react';
 import { AccidentTypeContext } from '../App.jsx';
 
 const Button = ({ data, i, j }) => {
-  // console.log(data, i);
   const [accidentInfo, setAccidentInfo] = useContext(AccidentTypeContext);
   return (
     <label>
       <input type="checkbox" className="selectBtn" checked={data.checked} onChange={() => {
-        // console.log(data, index);
         const newChecked = !data.checked;
         const newAccidentInfo = [...accidentInfo];
         newAccidentInfo[i].types[j].checked = newChecked;
-        // console.log(newAccidentInfo);
         setAccidentInfo(newAccidentInfo);
       }} />
       {data.type}
