6047c33260622c2c6ef2da6fc129ab2047f53ab7
aramackie <mackie1067@gmail.com>
2024-01-26 23:52:45 +0900

    アニメーション追加

---
+++ src/components/AnimationController.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react';
+import Slider from '@mui/material/Slider';
+import IconButton from '@mui/material/IconButton';
+import PauseRounded from '@mui/icons-material/PauseRounded';
+import PlayArrowRounded from '@mui/icons-material/PlayArrowRounded';
+
+function valueLabelFormat(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日`;
+}
+
+function AnimationController({ displayDate, setDisplayDate, animationPlayed, setAnimationPlayed, minDisplayDate, maxDisplayDate }) {
+  const animationStep = 24 * 60 * 60 * 1000;
+
+  useEffect(() => {
+    if (animationPlayed) {
+      const intervalId = setInterval(() => {
+        setDisplayDate(prevDisplayDate => (((prevDisplayDate + animationStep) - minDisplayDate) % (maxDisplayDate - minDisplayDate) + minDisplayDate));
+      }, 100);
+      return () => clearInterval(intervalId);
+    }
+  }, [animationPlayed]);
+
+  const marks = [
+    { value: minDisplayDate, label: valueLabelFormat(minDisplayDate) },
+    { value: maxDisplayDate, label: valueLabelFormat(maxDisplayDate) }
+  ];
+
+  const handleChange = (_, newValue) => {
+    setDisplayDate(newValue);
+  };
+
+  const handleClick = () => {
+    setAnimationPlayed(!animationPlayed);
+    if (displayDate === maxDisplayDate) {
+      setDisplayDate(minDisplayDate);
+    }
+  };
+
+  return (
+    <>
+      <Slider
+        value={displayDate}
+        valueLabelFormat={valueLabelFormat}
+        valueLabelDisplay="on"
+        step={animationStep}
+        marks={marks}
+        min={minDisplayDate}
+        max={maxDisplayDate}
+        onChange={handleChange}
+      />
+      <IconButton
+        aria-label={animationPlayed ? 'pause' : 'play'}
+        onClick={handleClick}
+        sx={{ bgcolor: '#1976d2', '&:hover': { bgcolor: '#1565c0' }, color: '#ffffff' }}
+      >
+        {
+          animationPlayed ? (
+            <PauseRounded sx={{ fontSize: '3rem' }} />
+          ) : (
+            <PlayArrowRounded sx={{ fontSize: '3rem' }} />
+          )
+        }
+      </IconButton>
+    </>
+  );
+}
+
+export default AnimationController;
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,17 @@
 import { MapContainer, TileLayer, Circle, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useState } from 'react';
-import Data from '../assets/data.json'
+import Data from '../assets/data.json';
+import AnimationController from './AnimationController';
+
+const dataByDate = {};
+Data.forEach((value) => {
+  const { ymd, ...other } = value;
+  if (dataByDate[ymd] === undefined) {
+    dataByDate[ymd] = [];
+  }
+  dataByDate[ymd].push(other);
+});
 
 function Dms2Deg(num) {
   return parseInt(num * 0.0000001) + parseInt(num * 0.00001) % 100 / 60 + num % 100000 * 0.001 / 60 / 60;
@@ -14,29 +24,61 @@ function Map() {
     [51.000000, 167.000000]  // 北端の座標
   ];
 
+  const [displayDate, setDisplayDate] = useState(new Date("2022-01-01").getTime());
+  const minDisplayDate = new Date("2022-01-01").getTime();
+  const maxDisplayDate = new Date("2022-12-31").getTime();
+
+  const [animationPlayed, setAnimationPlayed] = useState(false);
+
+  let circles;
+  if (animationPlayed) {
+    const displayDateObj = new Date(displayDate);
+    const str = `${displayDateObj.getFullYear()}-${("0" + displayDateObj.getMonth()+1).slice(-2)}-${("0" + displayDateObj.getDate()).slice(-2)}`;
+    if (dataByDate[str] === undefined) {
+      circles = <></>;
+    } else {
+      circles = dataByDate[str].map((item, index) => {
+        return (
+          <Circle center={[Dms2Deg(item.q), Dms2Deg(item.r)]} radius={10} key={index} color={item.b === 1 ? '#1762b6' : '#00dceb'}>
+            <Popup>{Dms2Deg(item.q)}, {Dms2Deg(item.r)}</Popup>
+          </Circle>
+        );
+      });
+    }
+  } else {
+    circles = Object.keys(dataByDate).map((key) => {
+      return dataByDate[key].map((item, index) => {
+        return (
+          <Circle center={[Dms2Deg(item.q), Dms2Deg(item.r)]} radius={10} key={key+index} color={item.b === 1 ? '#1762b6' : '#00dceb'}>
+            <Popup>{Dms2Deg(item.q)}, {Dms2Deg(item.r)}</Popup>
+          </Circle>
+        );
+      });
+    })
+  }
+
   return (
-    <MapContainer
-      center={japan}
-      zoom={5}
-      scrollWheelZoom={true}
-      maxBounds={japanBounds}
-    >
-      <TileLayer
-        bounds={japanBounds}
-        minZoom={5}
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {
-        Data.map((item, index) => {
-          return (
-            <Circle center={[Dms2Deg(item.q), Dms2Deg(item.r)]} radius={10} key={index}>
-              <Popup>{Dms2Deg(item.q)}, {Dms2Deg(item.r)}</Popup>
-            </Circle>
-          );
-        })
-      }
-    </MapContainer>
+    <div className="map-container">
+      <div className="map-wrapper">
+        <MapContainer
+          center={japan}
+          zoom={5}
+          scrollWheelZoom={true}
+          maxBounds={japanBounds}
+        >
+          <TileLayer
+            bounds={japanBounds}
+            minZoom={5}
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {circles}
+        </MapContainer>
+      </div>
+      <div className="controller-container">
+        <AnimationController　displayDate={displayDate} setDisplayDate={setDisplayDate} animationPlayed={animationPlayed} setAnimationPlayed={setAnimationPlayed} minDisplayDate={minDisplayDate} maxDisplayDate={maxDisplayDate} />
+      </div>
+    </div>
   );
 }
 
